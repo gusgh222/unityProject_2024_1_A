3ac3d9f5753d194a6e91326f20185450522da0f3
gusgh222 <hhh222@g.shingu.ac.kr>
2024-03-28 18:59:39 +0900

    [Feat] 이동과 충돌

---
+++ UnityProject_A_24_01/Assets/EH.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EH : MonoBehaviour
+{
+    public int Hp = 100;
+    public Text textUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        textUI.text = "체력 : " + Hp.ToString();
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Hp -= 10;
+            Debug.Log("체력 : " + Hp);
+            if(Hp <= 0)
+            {
+                textUI.text = "체력 : " + Hp.ToString();
+                Destroy(gameObject); 
+            }
+        }
+
+    }
+}
